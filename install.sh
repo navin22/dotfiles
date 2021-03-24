3be7161cf02c6e201c2cd04ea944aa5212a6a00e
Navin <navin22@github.com>
2021-03-24 22:12:13 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,2 @@
-set -x
-git config --global push.default current
-
-cd ~/
-
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc ~/.bashrc
 
-echo "" >> ~/.bashrc
-echo ". /workspaces/.codespaces/.persistedshare/dotfiles/bashprompt.sh" >> ~/.bashrc
