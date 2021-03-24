dc09fc29362b60249753d2be02e59b4c921f7eec
Navin <navb@microsoft.com>
2021-03-24 19:21:55 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ cd ~/
 
 echo "" >> ~/.bashrc
 echo ". ~/dotfiles/bashprompt.sh" >> ~/.bashrc
+
+ln -sv${LINK_TARGET_EXISTS_HANDLING} "${HOME}/.vscode.settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
