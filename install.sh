cb1a013b4c378ee3e0ba48fc50e797beb414c520
Navin <navin22@github.com>
2021-03-24 20:23:02 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,7 @@
 set -x
 git config --global push.default current
 
-cd ~/
+cd /workspaces/.codespaces/.persistedshare/dotfiles
 
 echo "" >> ~/.bashrc
 echo ". ~/dotfiles/bashprompt.sh" >> ~/.bashrc
-
-LINK_TARGET_EXISTS_HANDLING="f"
-ln -sv${LINK_TARGET_EXISTS_HANDLING} "${HOME}/.vscode.settings.json" "${HOME}/Library/Application Support/Code/User/settings.json"
