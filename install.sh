82a32c8cee63acb9be107d081ea87a88a86ec1c4
Navin <navin22@github.com>
2021-03-25 14:45:27 +0530

    added other sf

--- install.sh
+++ install.sh
@@ -1,2 +1,4 @@
 echo "Hello, world"
 ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.bashrc ~/.bashrc
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.vscode.settings.json ~/.vscode.settings.json
+ln -sf /workspaces/.codespaces/.persistedshare/dotfiles/.git-shortcuts ~/.git-shortcuts
